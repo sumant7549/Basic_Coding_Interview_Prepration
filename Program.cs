fae4945e21d2eea28e0605fd4d808a8faed6e3d6
sumant7549 <007kumarsumant@gmail.com>
2025-05-20 04:23:12 +0530

    FIrst Commit in the REpo

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Interview_Prepartion_All_basic_Coding;
+
+Console.WriteLine("Select a Program:");
+Console.WriteLine("1. Reverse String");
+
+
+Console.Write("Enter a choice: ");
+int choice = Convert.ToInt32(Console.ReadLine());
+
+
+switch (choice)
+{
+    case 1:
+        Console.Write("Enter a string to reverse: ");
+        string input = Console.ReadLine();
+        var reverseString = new ReverseString();
+        string reversed = reverseString.Reverse(input);
+        Console.WriteLine($"Reversed string: {reversed}");
+        break;
+
+    default:
+        Console.WriteLine("Invalid choice.");
+        break;
+}
+
+
---
+++ ReverseString.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interview_Prepartion_All_basic_Coding
+{
+    public class ReverseString
+    {
+        public string Reverse(string input)
+        {
+            char[] result = new char[input.Length];
+            for (int i = input.Length-1, j= 0; i >= 0; i--,j++)
+            {
+                result[j] = input[i];
+            }
+            //Array.Reverse(charArray);
+            return new string(result);
+        }
+    }
+}
